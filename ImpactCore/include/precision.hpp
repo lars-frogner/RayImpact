9771e1085867f2430789c696893eef59f6a46c7e
Lars Frogner <lars_frogner@hotmail.com>
2018-01-08 08:51:11 +0200

    Add typedef used by ErrorFloat

--- ImpactCore/include/precision.hpp
+++ ImpactCore/include/precision.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstdint>
+#include <limits>
 
 #pragma warning(disable: 4305) // truncation from 'double' to 'float'
 
@@ -8,8 +10,12 @@ namespace Impact {
 
 // Single precision
 typedef float imp_float;
+typedef uint32_t imp_float_bits;
 
 // Double precision
 //typedef double imp_float;
+//typedef uint64_t imp_float_bits;
+
+constexpr imp_float IMP_FLOAT_MAHCINE_EPS = std::numeric_limits<imp_float>::epsilon()/2;
 
 } // Impact
