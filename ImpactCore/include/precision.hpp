16b3861fe6340a8f2e5c61b0bcd5dda28aad4e35
Lars Frogner <lars_frogner@hotmail.com>
2018-01-08 22:02:10 +0200

    Use preprocessor definition do decide precision

--- ImpactCore/include/precision.hpp
+++ ImpactCore/include/precision.hpp
@@ -8,13 +8,15 @@ namespace Impact {
 
 // Typedefs for precision
 
+#ifndef IMP_FLOAT_IS_DOUBLE
 // Single precision
 typedef float imp_float;
 typedef uint32_t imp_float_bits;
-
+#else
 // Double precision
-//typedef double imp_float;
-//typedef uint64_t imp_float_bits;
+typedef double imp_float;
+typedef uint64_t imp_float_bits;
+#endif
 
 constexpr imp_float IMP_FLOAT_MAHCINE_EPS = std::numeric_limits<imp_float>::epsilon()/2;
 
