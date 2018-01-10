5aee7aeb8f912014e5f8ef03e25814e67ae64cc3
unknown <lars_frogner@hotmail.com>
2018-01-10 10:03:09 +0100

    Add model child class declarations

--- RayImpact/include/Model.hpp
+++ RayImpact/include/Model.hpp
@@ -1,8 +1,12 @@
 #pragma once
 #include "precision.hpp"
+#include "RegionAllocator.hpp"
 #include "BoundingBox.hpp"
 #include "Ray.hpp"
+#include "Transformation.hpp"
 #include "ScatteringEvent.hpp"
+#include "Shape.hpp"
+#include <memory>
 
 namespace Impact {
 namespace RayImpact {
@@ -29,8 +33,75 @@ public:
 	virtual const Material* getMaterial() const = 0;
 
 	virtual void computeScatteringFunctions(SurfaceScatteringEvent* scattering_event,
-											//MemoryArena& arena, TransportMode mode,
+											RegionAllocator& allocator,
+											//TransportMode mode,
 											bool allow_multiple_lobes) const = 0;
+};
+
+// GeometricModel declarations
+
+class GeometricModel : public Model {
+
+private:
+
+	std::shared_ptr<Shape> shape; // The shape associated with the model
+	std::shared_ptr<Material> material; // The material associated with the model
+	std::shared_ptr<AreaLight> area_light; // The area light (if any) associated with the model
+	MediumInterface medium_interface; // The interface between the media on the inside and the outside of the model
+
+public:
+
+	GeometricModel(const std::shared_ptr<Shape>& shape,
+				   const std::shared_ptr<Material>& material,
+				   const std::shared_ptr<AreaLight>& area_light,
+				   const MediumInterface& medium_interface);
+
+	BoundingBoxF worldSpaceBoundingBox() const;
+	
+	bool intersect(const Ray& ray,
+				   SurfaceScatteringEvent* scattering_event) const;
+	
+	bool hasIntersection(const Ray& ray) const;
+
+	const AreaLight* getAreaLight() const;
+
+	const Material* getMaterial() const;
+
+	void computeScatteringFunctions(SurfaceScatteringEvent* scattering_event,
+									RegionAllocator& allocator,
+									//TransportMode mode,
+									bool allow_multiple_lobes) const;
+};
+
+// TransformedModel declarations
+
+class TransformedModel : public Model {
+
+private:
+
+	std::shared_ptr<Model> model; // The underlying model for the transformed model
+	const Transformation model_to_world; // The transformation from the shape's notion of world space to actual world space
+
+public:
+
+	TransformedModel(const std::shared_ptr<Model>& model,
+				     const Transformation& model_to_world);
+
+	BoundingBoxF worldSpaceBoundingBox() const;
+	
+	bool intersect(const Ray& ray,
+				   SurfaceScatteringEvent* scattering_event) const;
+	
+	bool hasIntersection(const Ray& ray) const;
+
+	const AreaLight* getAreaLight() const;
+
+	const Material* getMaterial() const;
+
+	void computeScatteringFunctions(SurfaceScatteringEvent* scattering_event,
+									RegionAllocator& allocator,
+									//TransportMode mode,
+									bool allow_multiple_lobes) const;
 };
 
 } // RayImpact
