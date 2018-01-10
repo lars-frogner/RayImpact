ecd37ed7bec5d95f2d5eafbfd58a876aa7c30c84
unknown <lars_frogner@hotmail.com>
2018-01-10 08:49:19 +0100

    Move precision utility functions

--- ImpactCore/include/ErrorFloat.hpp
+++ ImpactCore/include/ErrorFloat.hpp
@@ -3,86 +3,11 @@
 #include "math.hpp"
 #include "string_util.hpp"
 #include "error.hpp"
-#include <cstring>
 #include <cmath>
 #include <algorithm>
 #include <ostream>
 
 namespace Impact {
-
-// Implement precision utility functions for float and double
-
-// Returns the 32/64-bit unsigned integer representing the bit pattern of the given float/double
-inline imp_float_bits floatToBits(imp_float value)
-{
-	imp_float_bits bits;
-	std::memcpy(&bits, &value, sizeof(imp_float));
-	return bits;
-}
-
-// Returns the float/double represented by the bit pattern of the given 32/64-bit unsigned integer
-inline imp_float bitsToFloat(imp_float_bits bits)
-{
-	imp_float value;
-	std::memcpy(&value, &bits, sizeof(imp_float_bits));
-	return value;
-}
-
-// Returns the closest float/double that is higher than the given float/double
-inline imp_float closestHigherFloat(imp_float value)
-{
-	if (std::isinf(value) && value > 0.0)
-	{
-		// No more values above positive infinity
-		return value;
-	}
-
-	// Negative zero must be converted to positive zero before continuing
-	if (value == -0.0)
-		value = 0.0;
-
-	imp_float_bits bits = floatToBits(value);
-
-	// Incrementing or decrementing the bit pattern yields
-	// the representation of the closest higher value
-	if (value >= 0.0)
-		bits++;
-	else
-		bits--;
-
-	return bitsToFloat(bits);
-}
-
-// Returns the closest float/double that is lower than the given float/double
-inline imp_float closestLowerFloat(imp_float value)
-{
-	if (std::isinf(value) && value < 0.0)
-	{
-		// No more values below negative infinity
-		return value;
-	}
-
-	// Positive zero must be converted to negative zero before continuing
-	if (value == 0.0)
-		value = -0.0;
-
-	imp_float_bits bits = floatToBits(value);
-	
-	// Incrementing or decrementing the bit pattern yields
-	// the representation of the closest lower value
-	if (value <= 0.0)
-		bits++;
-	else
-		bits--;
-
-	return bitsToFloat(bits);
-}
-
-// 1 + errorPowerBound(n) is a conservative bound on (1 +- machine_eps)^n
-inline constexpr imp_float errorPowerBound(int n)
-{
-	return (n*IMP_FLOAT_MAHCINE_EPS)/(1 - n*IMP_FLOAT_MAHCINE_EPS);
-}
 
 // ErrorFloat declarations
 
@@ -241,8 +166,8 @@ inline bool solveQuadraticEquation(const ErrorFloat& a,
 	                               ErrorFloat* x1,
 								   ErrorFloat* x2)
 {
-	assert(x1 && x2);
-	assert(a != 0);
+	imp_assert(x1 && x2);
+	imp_assert(a != 0);
 
 	const ErrorFloat& discriminant = b*b - ErrorFloat(4.0)*a*c;
 
@@ -518,13 +443,13 @@ inline void ErrorFloat::verifyBounds() const
 	if (!std::isinf(lower_bound) && !std::isinf(upper_bound) &&
 		!std::isnan(lower_bound) && !std::isnan(upper_bound))
 	{
-		assert(lower_bound <= upper_bound);
+		imp_assert(lower_bound <= upper_bound);
 	}
 
 	if (!std::isinf(value) && !std::isnan(value))
 	{
-		assert(precise_value <= upper_bound);
-		assert(precise_value >= lower_bound);
+		imp_assert(precise_value <= upper_bound);
+		imp_assert(precise_value >= lower_bound);
 	}
 	#endif
 }
--- ImpactCore/include/precision.hpp
+++ ImpactCore/include/precision.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <cstring>
 #include <limits>
 
 #pragma warning(disable: 4305) // truncation from 'double' to 'float'
@@ -19,5 +20,79 @@ typedef uint64_t imp_float_bits;
 #endif
 
 constexpr imp_float IMP_FLOAT_MAHCINE_EPS = std::numeric_limits<imp_float>::epsilon()/2;
+
+// Implement precision utility functions for float and double
+
+// Returns the 32/64-bit unsigned integer representing the bit pattern of the given float/double
+inline imp_float_bits floatToBits(imp_float value)
+{
+	imp_float_bits bits;
+	std::memcpy(&bits, &value, sizeof(imp_float));
+	return bits;
+}
+
+// Returns the float/double represented by the bit pattern of the given 32/64-bit unsigned integer
+inline imp_float bitsToFloat(imp_float_bits bits)
+{
+	imp_float value;
+	std::memcpy(&value, &bits, sizeof(imp_float_bits));
+	return value;
+}
+
+// Returns the closest float/double that is higher than the given float/double
+inline imp_float closestHigherFloat(imp_float value)
+{
+	if (std::isinf(value) && value > 0.0)
+	{
+		// No more values above positive infinity
+		return value;
+	}
+
+	// Negative zero must be converted to positive zero before continuing
+	if (value == -0.0)
+		value = 0.0;
+
+	imp_float_bits bits = floatToBits(value);
+
+	// Incrementing or decrementing the bit pattern yields
+	// the representation of the closest higher value
+	if (value >= 0.0)
+		bits++;
+	else
+		bits--;
+
+	return bitsToFloat(bits);
+}
+
+// Returns the closest float/double that is lower than the given float/double
+inline imp_float closestLowerFloat(imp_float value)
+{
+	if (std::isinf(value) && value < 0.0)
+	{
+		// No more values below negative infinity
+		return value;
+	}
+
+	// Positive zero must be converted to negative zero before continuing
+	if (value == 0.0)
+		value = -0.0;
+
+	imp_float_bits bits = floatToBits(value);
+	
+	// Incrementing or decrementing the bit pattern yields
+	// the representation of the closest lower value
+	if (value <= 0.0)
+		bits++;
+	else
+		bits--;
+
+	return bitsToFloat(bits);
+}
+
+// 1 + errorPowerBound(n) is a conservative bound on (1 +- machine_eps)^n
+inline constexpr imp_float errorPowerBound(int n)
+{
+	return (n*IMP_FLOAT_MAHCINE_EPS)/(1 - n*IMP_FLOAT_MAHCINE_EPS);
+}
 
 } // Impact
