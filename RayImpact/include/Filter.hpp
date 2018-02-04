e060d59d602c3d7e223be42630070a149b135138
Lars Frogner <lars_frogner@hotmail.com>
2018-02-04 08:55:39 +0200

    Move method implementations inside class

--- RayImpact/include/Filter.hpp
+++ RayImpact/include/Filter.hpp
@@ -14,17 +14,13 @@ public:
     Vector2F radius; // Half the full extent of the filter in each direction
     Vector2F inverse_radius; // Reciprocal of the radius vector
 
-    Filter(const Vector2F& radius);
+    Filter(const Vector2F& radius)
+        : radius(radius),
+          inverse_radius(1.0f/radius.x, 1.0f/radius.y)
+    {}
 
     virtual imp_float evaluate(const Point2F& position) const = 0;
 };
-
-// Filter method implementations
-
-inline Filter::Filter(const Vector2F& radius)
-    : radius(radius),
-      inverse_radius(1.0f/radius.x, 1.0f/radius.y)
-{}
 
 } // RayImpact
 } // Impact
