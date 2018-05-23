46e8a080272d09b2dcb4b4b81923372757fee56c
unknown <lars_frogner@hotmail.com>
2018-05-23 09:13:37 +0200

    Assume that given image is stored with top pixels at the beginning of the array

--- ImpactCore/include/image_util.hpp
+++ ImpactCore/include/image_util.hpp
@@ -1,11 +1,22 @@
 #pragma once
 #include "precision.hpp"
 #include <string>
+#include <cmath>
 
 namespace Impact {
 
+// Image utility function declarations
+
 bool writePFM(const std::string& filename,
               const imp_float* pixel_values,
-              unsigned int width, unsigned int height);
+              unsigned int width, unsigned int height,
+			  float pixel_scale);
+
+// Image utility inline function definitions
+
+inline float gammaEncoded(float pixel_value)
+{
+	return std::sqrt(pixel_value);
+}
 
 } // Impact
--- ImpactCore/src/image_util.cpp
+++ ImpactCore/src/image_util.cpp
@@ -2,12 +2,16 @@
 #include "error.hpp"
 #include <fstream>
 #include <memory>
+#include <algorithm>
 
 namespace Impact {
 
+// Image utility function definitions
+
 bool writePFM(const std::string& filename,
               const imp_float* pixel_values,
-              unsigned int width, unsigned int height)
+              unsigned int width, unsigned int height,
+			  float pixel_scale)
 {
     imp_check(pixel_values);
 
@@ -18,18 +22,26 @@ bool writePFM(const std::string& filename,
 
     unsigned int idx = 0;
     unsigned int pixel_idx;
+	float max_intensity = 0;
 
-    // Loop through image row by row from bottom to top and store values in output buffer
-    for (int j = (int)height - 1; j >= 0; j--) {
+    // Loop through image row by row from top (j = 0) to bottom (j = height) and store values in output buffer
+    for (int j = 0; j < (int)height; j++) {
         for (int i = 0; i < (int)width; i++)
         {
             pixel_idx = 3*(j*width + i);
 
-            output_buffer[idx++] = (float)pixel_values[pixel_idx    ];
-            output_buffer[idx++] = (float)pixel_values[pixel_idx + 1];
-            output_buffer[idx++] = (float)pixel_values[pixel_idx + 2];
+            output_buffer[idx++] = gammaEncoded((float)pixel_values[pixel_idx    ]);
+            output_buffer[idx++] = gammaEncoded((float)pixel_values[pixel_idx + 1]);
+            output_buffer[idx++] = gammaEncoded((float)pixel_values[pixel_idx + 2]);
+			max_intensity = std::max(max_intensity, output_buffer[idx-1] + output_buffer[idx-2] + output_buffer[idx-3]);
         }
     }
+
+	// Automatic normalization
+	if (pixel_scale < 0)
+		pixel_scale = 3/max_intensity;
+
+	scale *= pixel_scale;
 
     // Open file for output
     std::ofstream file;
