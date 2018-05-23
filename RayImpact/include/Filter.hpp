05d1b757b0ee4c5a2911cd4de449d3955d75cf54
unknown <lars_frogner@hotmail.com>
2018-05-23 08:57:27 +0200

    Move constructor definition out of class declaration

--- RayImpact/include/Filter.hpp
+++ RayImpact/include/Filter.hpp
@@ -14,13 +14,17 @@ public:
     Vector2F radius; // Half the full extent of the filter in each direction
     Vector2F inverse_radius; // Reciprocal of the radius vector
 
-    Filter(const Vector2F& radius)
-        : radius(radius),
-          inverse_radius(1.0f/radius.x, 1.0f/radius.y)
-    {}
+    Filter(const Vector2F& radius);
 
     virtual imp_float evaluate(const Point2F& position) const = 0;
 };
+
+// Filter inline method definitions
+
+inline Filter::Filter(const Vector2F& radius)
+    : radius(radius),
+      inverse_radius(1.0f/radius.x, 1.0f/radius.y)
+{}
 
 } // RayImpact
 } // Impact
