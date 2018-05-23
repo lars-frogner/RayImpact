9c5de91de7acd211cdfef2740df4e328ca971f34
unknown <lars_frogner@hotmail.com>
2018-05-23 08:59:37 +0200

    Move definitions out of class declaration

--- RayImpact/include/BilinearInterpolationTexture.hpp
+++ RayImpact/include/BilinearInterpolationTexture.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Texture.hpp"
+#include "ParameterSet.hpp"
 #include "error.hpp"
 #include <memory>
 
@@ -24,59 +25,54 @@ public:
 
     BilinearInterpolationTexture(const T& value_00, const T& value_01,
                                  const T& value_10, const T& value_11,
-                                 std::unique_ptr<TextureMapper2D> texture_mapper)
-        : value_00(value_00), value_01(value_01),
-          value_10(value_10), value_11(value_11),
-          mapper(std::move(texture_mapper))
-    {}
+                                 std::unique_ptr<TextureMapper2D> texture_mapper);
 
-    T evaluate(const SurfaceScatteringEvent& scattering_event) const
-    {
-        Vector2F dstdx, dstdy;
+    T evaluate(const SurfaceScatteringEvent& scattering_event) const;
 
-        const Point2F& coord = mapper->textureCoordinate(scattering_event, &dstdx, &dstdy);
-
-        return (1 - coord.x)*(1 - coord.y)*value_00 +
-               (1 - coord.x)*     coord.y *value_01 +
-                    coord.x *(1 - coord.y)*value_10 +
-                    coord.x *     coord.y *value_11;
-    }
+	std::string toString() const;
 };
 
-// BilinearInterpolationTexture creation
+// BilinearInterpolationTexture function declarations
 
 Texture<imp_float>* createBilinearInterpolationFloatTexture(const Transformation& texture_to_world,
-                                                            const TextureParameterSet& parameters)
-{
-    imp_float value_00 = parameters.getSingleFloatValue("value_00", 0.0f);
-    imp_float value_01 = parameters.getSingleFloatValue("value_01", 0.0f);
-    imp_float value_10 = parameters.getSingleFloatValue("value_10", 0.0f);
-    imp_float value_11 = parameters.getSingleFloatValue("value_11", 0.0f);
-
-    std::unique_ptr<TextureMapper2D> mapper;
-
-    create_2D_texture_mapper(parameters, mapper);
-
-    return new BilinearInterpolationTexture<imp_float>(value_00, value_01,
-                                                       value_10, value_11,
-                                                       std::move(mapper));
-}
+															const TextureParameterSet& parameters);
 
 Texture<Spectrum>* createBilinearInterpolationSpectrumTexture(const Transformation& texture_to_world,
-                                                              const TextureParameterSet& parameters)
+															  const TextureParameterSet& parameters);
+
+// BilinearInterpolationTexture inline method definitions
+
+template <typename T>
+inline BilinearInterpolationTexture<T>::BilinearInterpolationTexture(const T& value_00, const T& value_01,
+																     const T& value_10, const T& value_11,
+																     std::unique_ptr<TextureMapper2D> texture_mapper)
+    : value_00(value_00), value_01(value_01),
+      value_10(value_10), value_11(value_11),
+      mapper(std::move(texture_mapper))
+{}
+
+template <typename T>
+inline T BilinearInterpolationTexture<T>::evaluate(const SurfaceScatteringEvent& scattering_event) const
 {
-    const Spectrum& value_00 = parameters.getSingleSpectrumValue("value_00", Spectrum(0.0f));
-    const Spectrum& value_01 = parameters.getSingleSpectrumValue("value_01", Spectrum(0.0f));
-    const Spectrum& value_10 = parameters.getSingleSpectrumValue("value_10", Spectrum(0.0f));
-    const Spectrum& value_11 = parameters.getSingleSpectrumValue("value_11", Spectrum(0.0f));
+    Vector2F dstdx, dstdy;
 
-    std::unique_ptr<TextureMapper2D> mapper;
+    const Point2F& coord = mapper->textureCoordinate(scattering_event, &dstdx, &dstdy);
 
-    create_2D_texture_mapper(parameters, mapper);
+    return (1 - coord.x)*(1 - coord.y)*value_00 +
+           (1 - coord.x)*     coord.y *value_01 +
+                coord.x *(1 - coord.y)*value_10 +
+                coord.x *     coord.y *value_11;
+}
 
-    return new BilinearInterpolationTexture<Spectrum>(value_00, value_01,
-                                                      value_10, value_11,
-                                                      std::move(mapper));
+template <typename T>
+inline std::string BilinearInterpolationTexture<T>::toString() const
+{
+    std::ostringstream stream;
+	stream <<  "{value 00: " << value_00
+		   << ", value 01: " << value_01
+		   << ", value 10: " << value_10
+		   << ", value 11: " << value_11 << "}";
+    return stream.str();
 }
 
 } // RayImpact
