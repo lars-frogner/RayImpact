5aca545086e4295cb9606f7e948699117b89950c
unknown <lars_frogner@hotmail.com>
2018-05-23 08:39:13 +0200

    Move small methods to header file

--- RayImpact/include/Quaternion.hpp
+++ RayImpact/include/Quaternion.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include "precision.hpp"
-#include "math.hpp"
 #include "geometry.hpp"
 #include <ostream>
+#include <sstream>
+#include <string>
 
 namespace Impact {
 namespace RayImpact {
@@ -40,13 +41,121 @@ public:
     imp_float dot(const Quaternion& other) const;
 
     Quaternion normalized() const;
+
+	std::string toString() const;
 };
 
-// Functions on Quaternion objects
+// Quaternion function declarations
 
 Quaternion slerp(const Quaternion& quat_1, const Quaternion& quat_2, imp_float weight);
 
-std::ostream& operator<<(std::ostream& stream, const Quaternion& ray);
+// Quaternion inline method definitions
+
+inline Quaternion::Quaternion()
+    : imag(0, 0, 0), w(1)
+{}
+
+inline Quaternion::Quaternion(const Vector3F& imag, imp_float w)
+    : imag(imag), w(w)
+{}
+
+inline Quaternion::Quaternion(imp_float x, imp_float y, imp_float z, imp_float w)
+    : imag(x, y, z), w(w)
+{}
+
+inline Quaternion Quaternion::operator+(const Quaternion& other) const
+{
+    return Quaternion(imag + other.imag, w + other.w);
+}
+
+inline Quaternion Quaternion::operator-(const Quaternion& other) const
+{
+    return Quaternion(imag - other.imag, w - other.w);
+}
+
+inline Quaternion Quaternion::operator*(const Quaternion& other) const
+{
+    return Quaternion(imag.cross(other.imag) + other.imag*w + imag*other.w,
+                      w*other.w - imag.dot(other.imag));
+}
+
+inline Quaternion Quaternion::operator-() const
+{
+    return Quaternion(-imag, -w);
+}
+
+inline Quaternion Quaternion::operator*(imp_float factor) const
+{
+    return Quaternion(imag*factor, w*factor);
+}
+
+inline Quaternion Quaternion::operator/(imp_float divisor) const
+{
+    imp_float factor = 1.0f/divisor;
+    return Quaternion(imag*factor, w*factor);
+}
+
+inline Quaternion& Quaternion::operator+=(const Quaternion& other)
+{
+    imag += other.imag;
+    w += other.w;
+    return *this;
+}
+
+inline Quaternion& Quaternion::operator-=(const Quaternion& other)
+{
+    imag -= other.imag;
+    w -= other.w;
+    return *this;
+}
+
+inline Quaternion& Quaternion::operator*=(const Quaternion& other)
+{
+    imp_float imag_dot_prod = imag.dot(other.imag);
+    imag = imag.cross(other.imag) + other.imag*w + imag*other.w;
+    w = w*other.w - imag_dot_prod;
+    return *this;
+}
+
+inline Quaternion& Quaternion::operator*=(imp_float factor)
+{
+    imag *= factor;
+    w *= factor;
+    return *this;
+}
+
+inline Quaternion& Quaternion::operator/=(imp_float divisor)
+{
+    imp_float factor = 1.0f/divisor;
+    imag *= factor;
+    w *= factor;
+    return *this;
+}
+
+inline imp_float Quaternion::dot(const Quaternion& other) const
+{
+    return imag.dot(other.imag) + w*other.w;
+}
+
+inline Quaternion Quaternion::normalized() const
+{
+    return *this/dot(*this);
+}
+
+inline std::string Quaternion::toString() const
+{
+    std::ostringstream stream;
+	stream << "{imag: " << imag << ", w: " << w << "}";
+	return stream.str();
+}
+
+// Quaternion inline function definitions
+
+inline std::ostream& operator<<(std::ostream& stream, const Quaternion& quaternion)
+{
+    stream << quaternion.toString();
+    return stream;
+}
 
 } // RayImpact
 } // Impact
--- RayImpact/src/Quaternion.cpp
+++ RayImpact/src/Quaternion.cpp
@@ -5,102 +5,7 @@
 namespace Impact {
 namespace RayImpact {
 
-// Quaternion method implementations
-
-Quaternion::Quaternion()
-    : imag(0, 0, 0), w(1)
-{}
-
-Quaternion::Quaternion(const Vector3F& imag, imp_float w)
-    : imag(imag), w(w)
-{}
-
-Quaternion::Quaternion(imp_float x, imp_float y, imp_float z, imp_float w)
-    : imag(x, y, z), w(w)
-{}
-
-Quaternion Quaternion::operator+(const Quaternion& other) const
-{
-    return Quaternion(imag + other.imag, w + other.w);
-}
-
-Quaternion Quaternion::operator-(const Quaternion& other) const
-{
-    return Quaternion(imag - other.imag, w - other.w);
-}
-
-Quaternion Quaternion::operator*(const Quaternion& other) const
-{
-    return Quaternion(imag.cross(other.imag) + other.imag*w + imag*other.w,
-                      w*other.w - imag.dot(other.imag));
-}
-
-Quaternion Quaternion::operator-() const
-{
-    return Quaternion(-imag, -w);
-}
-
-Quaternion Quaternion::operator*(imp_float factor) const
-{
-    return Quaternion(imag*factor, w*factor);
-}
-
-Quaternion Quaternion::operator/(imp_float divisor) const
-{
-    imp_float factor = 1.0f/divisor;
-    return Quaternion(imag*factor, w*factor);
-}
-
-Quaternion& Quaternion::operator+=(const Quaternion& other)
-{
-    imag += other.imag;
-    w += other.w;
-    return *this;
-}
-
-Quaternion& Quaternion::operator-=(const Quaternion& other)
-{
-    imag -= other.imag;
-    w -= other.w;
-    return *this;
-}
-
-Quaternion& Quaternion::operator*=(const Quaternion& other)
-{
-    imp_float imag_dot_prod = imag.dot(other.imag);
-
-    imag = imag.cross(other.imag) + other.imag*w + imag*other.w;
-    w = w*other.w - imag_dot_prod;
-
-    return *this;
-}
-
-Quaternion& Quaternion::operator*=(imp_float factor)
-{
-    imag *= factor;
-    w *= factor;
-    return *this;
-}
-
-Quaternion& Quaternion::operator/=(imp_float divisor)
-{
-    imp_float factor = 1.0f/divisor;
-    imag *= factor;
-    w *= factor;
-    return *this;
-}
-
-imp_float Quaternion::dot(const Quaternion& other) const
-{
-    return imag.dot(other.imag) + w*other.w;
-}
-
-Quaternion Quaternion::normalized() const
-{
-    return *this/dot(*this);
-}
-
-// Functions on Quaternion objects
+// Quaternion function definitions
 
 // Spherical linear interpolation between the two quaternions
 Quaternion slerp(const Quaternion& quat_1, const Quaternion& quat_2, imp_float weight)
@@ -121,12 +26,6 @@ Quaternion slerp(const Quaternion& quat_1, const Quaternion& quat_2, imp_float w
 
         return quat_1*std::cos(weighted_theta) + orthogonal_quat*std::sin(weighted_theta);
     }
-}
-
-std::ostream& operator<<(std::ostream& stream, const Quaternion& quaternion)
-{
-    stream << "{imag = " << quaternion.imag << ", w = " << quaternion.w << "}";
-    return stream;
 }
 
 } // RayImpact
