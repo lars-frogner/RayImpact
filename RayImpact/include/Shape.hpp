71512524a5a90a6ec3a0481923fa641c8421a64e
unknown <lars_frogner@hotmail.com>
2018-05-23 08:36:34 +0200

    Move large function from header to source file

--- RayImpact/include/Shape.hpp
+++ RayImpact/include/Shape.hpp
@@ -38,36 +38,31 @@ public:
     virtual imp_float surfaceArea() const = 0;
 };
 
-// Utility function for shapes
+// Shape function declarations
 
 // Computes derivatives of the surface normal with respect to the surface parameters
-inline void computeNormalDerivatives(const Vector3F& dpdu,
-                                     const Vector3F& dpdv,
-                                     const Vector3F& d2pdu2,
-                                     const Vector3F& d2pdudv,
-                                     const Vector3F& d2pdv2,
-                                     Normal3F* dndu,
-                                     Normal3F* dndv)
+void computeNormalDerivatives(const Vector3F& dpdu,
+                              const Vector3F& dpdv,
+                              const Vector3F& d2pdu2,
+                              const Vector3F& d2pdudv,
+                              const Vector3F& d2pdv2,
+                              Normal3F* dndu,
+                              Normal3F* dndv);
+
+// Shape inline method definitions
+
+inline Shape::Shape(const Transformation* object_to_world,
+					const Transformation* world_to_object,
+					bool has_reverse_orientation)
+    : object_to_world(object_to_world),
+      world_to_object(world_to_object),
+      has_reverse_orientation(has_reverse_orientation),
+      transformation_swaps_handedness(object_to_world->swapsHandedness())
+{}
+
+inline BoundingBoxF Shape::worldSpaceBoundingBox() const
 {
-    // Uses the Weingarten equations
-
-    imp_float E = dpdu.squaredLength();
-    imp_float F = dpdu.dot(dpdv);
-    imp_float G = dpdv.squaredLength();
-
-    const Vector3F& surface_normal = dpdu.cross(dpdv).normalized();
-
-    imp_float e = surface_normal.dot(d2pdu2);
-    imp_float f = surface_normal.dot(d2pdudv);
-    imp_float g = surface_normal.dot(d2pdv2);
-
-    imp_float normal_deriv_norm = 1.0f/(E*G - F*F);
-
-    *dndu = Normal3F(dpdu*((f*F - e*G)*normal_deriv_norm) +
-                     dpdv*((e*F - f*E)*normal_deriv_norm));
-
-    *dndv = Normal3F(dpdu*((g*F - f*G)*normal_deriv_norm) +
-                     dpdv*((f*F - g*E)*normal_deriv_norm));
+    return (*object_to_world)(objectSpaceBoundingBox());
 }
 
 } // RayImpact
--- RayImpact/src/Shape.cpp
+++ RayImpact/src/Shape.cpp
@@ -3,21 +3,7 @@
 namespace Impact {
 namespace RayImpact {
 
-// Shape method implementations
-
-Shape::Shape(const Transformation* object_to_world,
-             const Transformation* world_to_object,
-             bool has_reverse_orientation)
-    : object_to_world(object_to_world),
-      world_to_object(world_to_object),
-      has_reverse_orientation(has_reverse_orientation),
-      transformation_swaps_handedness(object_to_world->swapsHandedness())
-{}
-
-BoundingBoxF Shape::worldSpaceBoundingBox() const
-{
-    return (*object_to_world)(objectSpaceBoundingBox());
-}
+// Shape method definitions
 
 bool Shape::hasIntersection(const Ray& ray,
                             bool test_alpha_texture /* = true */) const
@@ -26,6 +12,38 @@ bool Shape::hasIntersection(const Ray& ray,
     SurfaceScatteringEvent scattering_event;
 
     return intersect(ray, &intersection_distance, &scattering_event, test_alpha_texture);
+}
+
+// Shape function definitions
+
+// Computes derivatives of the surface normal with respect to the surface parameters
+void computeNormalDerivatives(const Vector3F& dpdu,
+                              const Vector3F& dpdv,
+                              const Vector3F& d2pdu2,
+                              const Vector3F& d2pdudv,
+                              const Vector3F& d2pdv2,
+                              Normal3F* dndu,
+                              Normal3F* dndv)
+{
+    // Uses the Weingarten equations
+
+    imp_float E = dpdu.squaredLength();
+    imp_float F = dpdu.dot(dpdv);
+    imp_float G = dpdv.squaredLength();
+
+    const Vector3F& surface_normal = dpdu.cross(dpdv).normalized();
+
+    imp_float e = surface_normal.dot(d2pdu2);
+    imp_float f = surface_normal.dot(d2pdudv);
+    imp_float g = surface_normal.dot(d2pdv2);
+
+    imp_float normal_deriv_norm = 1.0f/(E*G - F*F);
+
+    *dndu = Normal3F(dpdu*((f*F - e*G)*normal_deriv_norm) +
+                     dpdv*((e*F - f*E)*normal_deriv_norm));
+
+    *dndv = Normal3F(dpdu*((g*F - f*G)*normal_deriv_norm) +
+                     dpdv*((f*F - g*E)*normal_deriv_norm));
 }
 
 } // RayImpact
